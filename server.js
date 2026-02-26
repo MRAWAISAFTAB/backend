19fa91219484d9b9e1d640823310c11bc9454273
MRAWAISAFTAB <aftabawais36@gmail.com>
2026-02-26 19:45:18 +0500

    fix cors

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-const cookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser")
 const cors = require("cors");
 const path = require("path");
-const connectDB = require('./config/db');
+const connectDB = require('./config/db')
 const authRoutes = require('./routes/authRoutes');
 const incomeRoutes = require("./routes/incomeRoutes");
 const expenseRoutes = require("./routes/expenseRoutes");
@@ -11,27 +11,37 @@ const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 
+// Middleware to handle image uploads
+// app.use("/api/upload", uploadRoutes);
+
+
+// Middleware to handle cors
+
+// const cors = require("cors");
+
 app.use(cors({
   origin: "https://frontend-two-xi-28.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET","POST","PUT","DELETE"],
+  allowedHeaders: ["Content-Type","Authorization"],
   credentials: true
 }));
 
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParser())
 
-connectDB();
+connectDB()
 
-app.use("/api", authRoutes);
-app.use("/api/income", incomeRoutes);
-app.use("/api/expense", expenseRoutes);
-app.use("/api/dashboard", dashboardRoutes); // ✅ fixed
+app.use("/api", authRoutes)
+app.use("/api/income", incomeRoutes)
+app.use("/api/expense", expenseRoutes)
+app.use("/dashboard", dashboardRoutes)
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.get("/", (req, res) => {
-  res.send("hello world!");
-});
+app.get("/" ,(req,res ) =>{
+  res.send("hellow worlds!")
+})
 
-// ✅ Required for Vercel — export instead of listen
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
+})
